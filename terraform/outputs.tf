f07a014d8f219a60d26c14f00df7d77492d79500
mmdcloud <madmaxcloudpractice@gmail.com>
2025-12-11 16:38:04 +0530

    update: remove commented-out Debezium Connect service URL and update AWS provider version

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -14,9 +14,3 @@ output "msk_bootstrap_brokers_plaintext" {
   description = "Plaintext connection host:port pairs for the MSK brokers"
   value       = module.msk_cluster.bootstrap_brokers
 }
-
-# ECS Outputs
-# output "debezium_connect_service_url" {
-#   description = "URL for the Debezium Connect service"
-#   value       = "http://${module.debezium_connect_ecs.service_discovery_dns}:8083"
-# }
